076fc02384fa9bb6731d00396c85154a865193ed
tornike thcalidze <ttchalidze@gmail.com>
2025-03-30 23:21:45 -0400

    first1

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,25 @@
+import useGame from "./context";
+
 export default function App() {
   return <></>;
+}
+
+function field() {
+  const { feild } = useGame();
+  return (
+    <ul style={{ fisplay: "grid", gridTemplateColumns: "repeat(3,1fr)" }}>
+      {feild.map(isMole ? <Mole /> : <Hole />)}
+    </ul>
+  );
+}
+function Mole() {
+  const { whack } = useGame();
+  return (
+    <p style={{ border: "1px solid red" }} onClick={() => whack()}>
+      Mole
+    </p>
+  );
+}
+function Hole() {
+  return <p>Hole</p>;
 }
